1834acc60ed97c683c4f4c45ae8824d3a65bb926
atribolt <atribolt@gmail.com>
2022-05-21 00:02:53 +0300

    Create repo
    - added script for remove files by tree from folder

---
+++ remove-by-folder.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+
+import os
+from argparse import ArgumentParser
+
+
+parser = ArgumentParser()
+parser.add_argument('-s', '--source', help='Путь содержащий иерархию, которую нужно удалить')
+parser.add_argument('-r', '--remove', help='Путь, относительно которого будут удаляться файлы')
+
+
+args = parser.parse_args()
+args.source = os.path.abspath(args.source)
+args.remove = os.path.abspath(args.remove)
+
+
+assert os.path.exists(args.source), f'Путь "{args.source}" не существует'
+assert os.path.exists(args.remove), f'Путь "{args.remove}" не сущетсвует'
+
+
+source_list = []
+length = len(args.source) + 1
+for root, dirs, files in os.walk(args.source, True):
+  root = os.path.join(args.remove, root[length:])
+  if files:
+    [source_list.append(os.path.join(root, file)) for file in files]
+
+
+for file in source_list:
+  try:
+    os.remove(file)
+  except Exception as ex:
+    print(ex)
